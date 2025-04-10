a42cf3b89b2a088bcf7a9cec8d0d9c9a86993b08
bazhenov-denis <denis.bazhenov2005@yandex.ru>
2025-04-10 17:52:31 +0300

    init project

---
+++ kpo-backend/src/main/java/com/example/kpo_backend/KpoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kpo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpoBackendApplication.class, args);
+	}
+
+}
